f4d066e6a4fb25b8ff759f17a9b5d8404d62991e
Matheus Marchini <mat@mmarchini.me>
2019-01-08 11:30:34 -0200

    ignore undefined name _

--- pyflakes/checker.py
+++ pyflakes/checker.py
@@ -502,7 +502,7 @@ class DoctestScope(ModuleScope):
 
 # Globally defined names which are not attributes of the builtins module, or
 # are only present on some platforms.
-_MAGIC_GLOBALS = ['__file__', '__builtins__', 'WindowsError']
+_MAGIC_GLOBALS = ['__file__', '__builtins__', 'WindowsError', '_']
 # module scope annotation will store in `__annotations__`, see also PEP 526.
 if PY36_PLUS:
     _MAGIC_GLOBALS.append('__annotations__')
